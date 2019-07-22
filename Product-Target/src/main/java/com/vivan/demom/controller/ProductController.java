1981b1763a7f6b9ee9088f9b3ca121410153d5de
Vivan Manoj Dsouza <a8422@amberroad.com>
2019-07-22 18:59:43 +0530

    updated with logs

--- Product-Target/src/main/java/com/vivan/demom/controller/ProductController.java
+++ Product-Target/src/main/java/com/vivan/demom/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.vivan.demom.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,108 +23,117 @@ import com.vivan.demom.validators.ApiValidator;
 
 @RestController
 public class ProductController {
-	
+
+	public static Logger m_Logger = LoggerFactory.getLogger(ProductController.class);
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Autowired
 	private ApiValidator apivalidator;
 
 	public void setProductService(ProductService productService) {
 		this.productService = productService;
 	}
-	
-	 @GetMapping("/products")
-	 public List<Products> getEmployees() {
-	  List<Products> products = productService.retriveProducts();
-	  return products;
-	 }
-	 
-	 @GetMapping("/products/{department}")
-	 public List<Products> getEmployees(@PathVariable(name="department")String department) {
-	  List<Products> products = productRepository.findByDepartment(department);
-	  return products;
-	 }
-	 
-	 @GetMapping("/product/{company}")
-	 public List<Products> getEmployeesCompany(@PathVariable(name="company")String company) {
-	  List<Products> products = productRepository.findByCompany(company);
-	  return products;
-	 }
-	 
-	 @GetMapping("/product/cmp/{product_title}")
-	 public List<Products> getEmployeesTitle(@PathVariable(name="product_title")String product_title) {
-	  List<Products> products = productRepository.findByProduct_Title(product_title);
-	  return products;
-	 }
-	
-	 @PostMapping("/products")
-	 public ResponseEntity<APIResponse> saveEmployee(@RequestBody Products product){
-		 boolean isValidInput=apivalidator.isValidProduct(product);
-		 
-		 APIResponse apiResponse=new APIResponse();
-		 try {
-		 if(isValidInput) {
-			 	productService.saveProduct(product);
-			 	apiResponse.setStatus("000");
-		 		apiResponse.setMessage("Successfully created product "+product.getProduct_id());
-		 		System.out.println("Product saved Successfully");
-	    	}else {
-	    		apiResponse.setStatus("400");
-	    		apiResponse.setMessage("Please specify the product details");
-	    		return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.BAD_REQUEST);
-	    	}
-		 
-		 } catch(Exception ex) {
-		    	apiResponse.setStatus("404");
-				apiResponse.setMessage(ex.getMessage());
-		    	return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.NOT_FOUND);
-		    }
-		 
-		return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.OK);
-	 }
-	 
-	 @DeleteMapping("/products/{id}")
-	 public ResponseEntity<APIResponse> deleteEmployee(@PathVariable(name="id")long productId){
-		 APIResponse apiResponse=new APIResponse();
-		 try {
-		 productService.deleteProduct(productId);
-		 apiResponse.setStatus("000");
-	 	 apiResponse.setMessage("Successfully Deleted  "+productId);
-	 		System.out.println("Product saved Successfully");
-		 } catch(Exception ex) {
-		    apiResponse.setStatus("404");
-			apiResponse.setMessage(ex.getMessage());
-		    return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.NOT_FOUND);
-		    }
-		 return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.OK);
-	 }
-	 
-	 @PutMapping("/products/{id}")
-	 public ResponseEntity<APIResponse> updateEmployee(@RequestBody Products products,
-	   @PathVariable(name="id")Long productId){
-		 APIResponse apiResponse=new APIResponse();
+
+	@GetMapping("/products")
+	public List<Products> getProducts() {
+		List<Products> products = productService.retriveProducts();
+		return products;
+	}
+
+	@GetMapping("/products/{department}")
+	public List<Products> getProducts(@PathVariable(name = "department") String department) {
+		List<Products> products = productRepository.findByDepartment(department);
+		return products;
+	}
+
+	@GetMapping("/product/{company}")
+	public List<Products> getProductCompany(@PathVariable(name = "company") String company) {
+		List<Products> products = productRepository.findByCompany(company);
+		return products;
+	}
+
+	@GetMapping("/product/cmp/{product_title}")
+	public List<Products> getProductTitle(@PathVariable(name = "product_title") String product_title) {
+		List<Products> products = productRepository.findByProduct_Title(product_title);
+		return products;
+	}
+
+	@PostMapping("/products")
+	public ResponseEntity<APIResponse> saveProduct(@RequestBody Products product) {
+		boolean isValidInput = apivalidator.isValidProduct(product);
+		m_Logger.debug("Entering inside the saveProduct method ");
+		APIResponse apiResponse = new APIResponse();
 		try {
-		 Products prod = productService.getProduct(productId);
-		 if(prod != null){
-			 products.setProduct_id(productId);
-			 productService.updateProduct(products);
-			 apiResponse.setStatus("000");
-		 	 apiResponse.setMessage("Successfully updated  "+productId);
-		 }else {
-			 apiResponse.setStatus("405");
-			 apiResponse.setMessage("Mentioned ProductId   "+productId+" Not Found");
-			 return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.NOT_FOUND);
-		 }
-		 } catch(Exception ex) {
-			 	apiResponse.setStatus("404");
-				apiResponse.setMessage(ex.getMessage());
-			    return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.NOT_FOUND);
-		 }
-		 return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.OK);
-	 }
+			if (isValidInput) {
+				productService.saveProduct(product);
+				apiResponse.setStatus("000");
+				apiResponse.setMessage("Successfully created product " + product.getProduct_id());
+			} else {
+				apiResponse.setStatus("400");
+				apiResponse.setMessage("Please specify the product details");
+				return new ResponseEntity<APIResponse>(apiResponse, HttpStatus.BAD_REQUEST);
+			}
+
+		} catch (Exception ex) {
+			apiResponse.setStatus("404");
+			apiResponse.setMessage(ex.getMessage());
+			m_Logger.error("Exception in [saveProduct] Method while saving the product id " + product.getProduct_id()
+					+ " Reason" + ex);
+			return new ResponseEntity<APIResponse>(apiResponse, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<APIResponse>(apiResponse, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<APIResponse> deleteProduct(@PathVariable(name = "id") long productId) {
+		m_Logger.debug("Entering inside the deleteProduct method ");
+		APIResponse apiResponse = new APIResponse();
+		try {
+			productService.deleteProduct(productId);
+			apiResponse.setStatus("000");
+			apiResponse.setMessage("Successfully Deleted  " + productId);
+			System.out.println("Product saved Successfully");
+		} catch (Exception ex) {
+			apiResponse.setStatus("404");
+			apiResponse.setMessage(ex.getMessage());
+			m_Logger.error(
+					"Exception in [deleteProduct] Method while saving the product id " + productId + " Reason" + ex);
+			return new ResponseEntity<APIResponse>(apiResponse, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<APIResponse>(apiResponse, HttpStatus.OK);
+	}
+
+	@PutMapping("/products/{id}")
+	public ResponseEntity<APIResponse> updateProduct(@RequestBody Products products,
+			@PathVariable(name = "id") Long productId) {
+		APIResponse apiResponse = new APIResponse();
+		try {
+			m_Logger.debug("Entering inside the updateProduct method ");
+			Products prod = productService.getProduct(productId);
+			if (prod != null) {
+				products.setProduct_id(productId);
+				productService.updateProduct(products);
+				apiResponse.setStatus("000");
+				apiResponse.setMessage("Successfully updated  " + productId);
+			} else {
+				apiResponse.setStatus("405");
+				apiResponse.setMessage("Mentioned ProductId   " + productId + " Not Found");
+				return new ResponseEntity<APIResponse>(apiResponse, HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception ex) {
+			apiResponse.setStatus("404");
+			apiResponse.setMessage(ex.getMessage());
+			m_Logger.error(
+					"Exception in [updateProduct] Method while saving the product id " + productId + " Reason" + ex);
+			return new ResponseEntity<APIResponse>(apiResponse, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<APIResponse>(apiResponse, HttpStatus.OK);
+	}
 
 }
